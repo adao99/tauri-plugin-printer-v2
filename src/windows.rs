118e544491511b842b73d7038acd19af295f3378
Diogo Adão <dev_diogo.addo@hotmail.com>
2024-06-30 20:20:53 +0100

    ported to tauri beta.22

--- src/windows.rs
+++ src/windows.rs
@@ -1,11 +1,10 @@
 
 
-// use std::process::{Command, Stdio};
+use std::process::{Command, Output, Stdio};
 use std::{sync::mpsc, io::Write};
 use std::thread;
 use std::fs::File;
 use std::env;
-use tauri::api::process::Command;
 use crate::{declare::PrintOptions, fsys::remove_file};
 /**
  * Create sm.exe to temp
@@ -40,9 +39,14 @@ pub fn get_printers() -> String {
 
     // Spawn a new thread
     thread::spawn(move || {
-        let output: tauri::api::process::Output = Command::new("powershell").args(["Get-Printer | Select-Object Name, DriverName, JobCount, PrintProcessor, PortName, ShareName, ComputerName, PrinterStatus, Shared, Type, Priority | ConvertTo-Json"]).output().unwrap();
+        // let output: tauri_plugin_shell::process::Output = Command::new("powershell").args(["Get-Printer | Select-Object Name, DriverName, JobCount, PrintProcessor, PortName, ShareName, ComputerName, PrinterStatus, Shared, Type, Priority | ConvertTo-Json"]).output().unwrap();
 
-        sender.send(output.stdout.to_string()).unwrap();
+        let output = Command::new("powershell").args(["Get-Printer | Select-Object Name, DriverName, JobCount, PrintProcessor, PortName, ShareName, ComputerName, PrinterStatus, Shared, Type, Priority | ConvertTo-Json"]).output().unwrap();
+
+
+
+
+        sender.send(String::from_utf8(output.stdout).unwrap()).unwrap();
     });
 
     // Do other non-blocking work on the main thread
@@ -51,15 +55,19 @@ pub fn get_printers() -> String {
     let result: String = receiver.recv().unwrap();
 
 
-    return result
+    return result;
 }
 
 /**
  * Get printers by name on windows using powershell
  */
 pub fn get_printers_by_name(printername: String) -> String {
+    // let output = Command::new("powershell").args([format!("Get-Printer -Name \"{}\" | Select-Object Name, DriverName, JobCount, PrintProcessor, PortName, ShareName, ComputerName, PrinterStatus, Shared, Type, Priority | ConvertTo-Json", printername)]).output().unwrap();
+    // return output.stdout.to_string();
+
     let output = Command::new("powershell").args([format!("Get-Printer -Name \"{}\" | Select-Object Name, DriverName, JobCount, PrintProcessor, PortName, ShareName, ComputerName, PrinterStatus, Shared, Type, Priority | ConvertTo-Json", printername)]).output().unwrap();
-    return output.stdout.to_string();
+
+    return String::from_utf8(output.stdout).unwrap();
 }
 
 /**
@@ -80,9 +88,13 @@ pub fn print_pdf (options: PrintOptions) -> String {
     println!("{}", shell_command);
     // Spawn a new thread
     thread::spawn(move || {
-        let output: tauri::api::process::Output = Command::new("powershell").args([shell_command]).output().unwrap();
+        // let output: tauri_plugin_shell::process::Output = Command::new("powershell").args([shell_command]).output().unwrap();
 
-        sender.send(output.stdout.to_string()).unwrap();
+        // sender.send(output.stdout.to_string()).unwrap();
+
+        let output = Command::new("powershell").args([shell_command]).output().unwrap();
+
+        sender.send(String::from_utf8(output.stdout).unwrap()).unwrap();
     });
 
     // Do other non-blocking work on the main thread
@@ -104,16 +116,22 @@ pub fn print_pdf (options: PrintOptions) -> String {
  * Get printer job on windows using powershell
  */
 pub fn get_jobs(printername: String) -> String {
+    // let output = Command::new("powershell").args([format!("Get-PrintJob -PrinterName \"{}\"  | Select-Object DocumentName,Id,TotalPages,Position,Size,SubmmitedTime,UserName,PagesPrinted,JobTime,ComputerName,Datatype,PrinterName,Priority,SubmittedTime,JobStatus | ConvertTo-Json", printername)]).output().unwrap();
+    // return output.stdout.to_string();
+
     let output = Command::new("powershell").args([format!("Get-PrintJob -PrinterName \"{}\"  | Select-Object DocumentName,Id,TotalPages,Position,Size,SubmmitedTime,UserName,PagesPrinted,JobTime,ComputerName,Datatype,PrinterName,Priority,SubmittedTime,JobStatus | ConvertTo-Json", printername)]).output().unwrap();
-    return output.stdout.to_string();
+    return String::from_utf8(output.stdout).unwrap();
 }
 
 /**
  * Get printer job by id on windows using powershell
  */
 pub fn get_jobs_by_id(printername: String, jobid: String) -> String {
+    // let output = Command::new("powershell").args([format!("Get-PrintJob -PrinterName \"{}\" -ID \"{}\"  | Select-Object DocumentName,Id,TotalPages,Position,Size,SubmmitedTime,UserName,PagesPrinted,JobTime,ComputerName,Datatype,PrinterName,Priority,SubmittedTime,JobStatus | ConvertTo-Json", printername, jobid)]).output().unwrap();
+    // return output.stdout.to_string();
+
     let output = Command::new("powershell").args([format!("Get-PrintJob -PrinterName \"{}\" -ID \"{}\"  | Select-Object DocumentName,Id,TotalPages,Position,Size,SubmmitedTime,UserName,PagesPrinted,JobTime,ComputerName,Datatype,PrinterName,Priority,SubmittedTime,JobStatus | ConvertTo-Json", printername, jobid)]).output().unwrap();
-    return output.stdout.to_string();
+    return String::from_utf8(output.stdout).unwrap();
 }
 
 
@@ -121,30 +139,41 @@ pub fn get_jobs_by_id(printername: String, jobid: String) -> String {
  * Resume printers job on windows using powershell
  */
 pub fn resume_job(printername: String, jobid: String) -> String {
+    // let output = Command::new("powershell").args([format!("Resume-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
+    // return output.stdout.to_string();
+
     let output = Command::new("powershell").args([format!("Resume-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
-    return output.stdout.to_string();
+    return String::from_utf8(output.stdout).unwrap();
 }
 
 /**
  * Restart printers job on windows using powershell
  */
 pub fn restart_job(printername: String, jobid: String) -> String {
+    // let output = Command::new("powershell").args([format!("Restart-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
+    // return output.stdout.to_string();
+
     let output = Command::new("powershell").args([format!("Restart-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
-    return output.stdout.to_string();
+    return String::from_utf8(output.stdout).unwrap();
 }
 
 /**
  * pause printers job on windows using powershell
  */
 pub fn pause_job(printername: String, jobid: String) -> String {
+    // let output = Command::new("powershell").args([format!("Suspend-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
+    // return output.stdout.to_string();
+
     let output = Command::new("powershell").args([format!("Suspend-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
-    return output.stdout.to_string();
+    return String::from_utf8(output.stdout).unwrap();
 }
 
 /**
  * remove printers job on windows using powershell
  */
 pub fn remove_job(printername: String, jobid: String) -> String {
+    // let output = Command::new("powershell").args([format!("Remove-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
+    // return output.stdout.to_string();
     let output = Command::new("powershell").args([format!("Remove-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
-    return output.stdout.to_string();
+    return String::from_utf8(output.stdout).unwrap();
 }
